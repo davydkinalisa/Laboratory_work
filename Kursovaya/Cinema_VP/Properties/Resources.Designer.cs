a9534be7e1b1ca2a220a26076ddc65a155ee0b4b
davydkinalisa <62310654+davydkinalisa@users.noreply.github.com>
2020-05-25 00:44:08 +0300

    ReadyKursovaya

--- Kursovaya/Cinema_VP/Properties/Resources.Designer.cs
+++ Kursovaya/Cinema_VP/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Cinema_VP.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap Ticket_maket {
+            get {
+                object obj = ResourceManager.GetObject("Ticket_maket", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap User_Admin {
             get {
                 object obj = ResourceManager.GetObject("User_Admin", resourceCulture);
@@ -76,6 +86,16 @@ namespace Cinema_VP.Properties {
         public static System.Drawing.Bitmap User_Guest {
             get {
                 object obj = ResourceManager.GetObject("User_Guest", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap User_Guest1 {
+            get {
+                object obj = ResourceManager.GetObject("User_Guest1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
